3d874eb62c5a3a1056435a2ce536ead1360e6acd
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2023-07-30 17:15:43 +0300

    feat: change img tag to Image component

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -6,12 +6,13 @@ import HStack from "@uikit/Stack/HStack";
 import Heading from "@uikit/Heading";
 import Card from "@uikit/Card";
 import styles from "./index.module.scss";
+import Image from "next/image";
 
 const ProjectCard = () => {
   return (
     <Card>
       <div className={styles.projectImage}>
-        <img className={styles.image} src={"https://mobimg.b-cdn.net/v3/fetch/06/06fc18c093d3706106e26338f040a3fa.jpeg?w=1470&r=0.5625"} alt={""}/>
+        <Image className={styles.image} src={"https://mobimg.b-cdn.net/v3/fetch/06/06fc18c093d3706106e26338f040a3fa.jpeg?w=1470&r=0.5625"} alt={""}/>
         <div className={styles.readMore}>
           <VStack className={styles.readMoreBody} spacing={1}>
             <Text s={"xl"}><BsBoxArrowRight/></Text>
